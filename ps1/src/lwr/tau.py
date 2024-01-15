3189ebdceaa04aaf890c925d1a7d6a174950995e
ultraspicy <element.no.88@gmail.com>
2024-01-14 22:28:43 -0800

    Add code solution and written solution for ps1 q2 (c)

--- ps1/src/lwr/tau.py
+++ ps1/src/lwr/tau.py
@@ -15,14 +15,53 @@ def main(tau_values, train_path, valid_path, test_path, pred_path):
         test_path: Path to CSV file containing test set.
         pred_path: Path to save predictions.
     """
+    # *** START CODE HERE ***
+
     # Load training set
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    # Load validation set
+    x_valid, y_valid = util.load_dataset(valid_path, add_intercept=True)
+    # Load test set
+    x_test, y_test = util.load_dataset(test_path, add_intercept=True)
+    
+    # Initialize variables to store the best tau and its MSE
+    best_tau = None
+    best_mse = float('inf')
 
-    # *** START CODE HERE ***
     # Search tau_values for the best tau (lowest MSE on the validation set)
+    for tau in tau_values:
+        # Fit LWR model
+        model = LocallyWeightedLinearRegression(tau)
+        model.fit(x_train, y_train)
+        # Make predictions on validation set
+        y_valid_pred = model.predict(x_valid)
+        # Calculate MSE on validation set
+        mse = np.mean((y_valid - y_valid_pred) ** 2)
+        # Update best tau if this is the lowest MSE so far
+        if mse < best_mse:
+            best_mse = mse
+            best_tau = tau
+        plt.figure(f"tau = {tau}")
+        plt.plot(x_train[:, 1], y_train, 'bx', label='Training Data')
+        plt.plot(x_valid[:, 1], y_valid, 'go', label='Validation Data')
+        plt.plot(x_valid[:, 1], y_valid_pred, 'ro', label='LWR Predictions')
+        plt.legend()
+        plt.show()
+            
     # Fit a LWR model with the best tau value
+    model = LocallyWeightedLinearRegression(best_tau)
+    model.fit(x_train, y_train)
     # Run on the test set to get the MSE value
-    # Save predictions to pred_path
+    y_test_pred = model.predict(x_test)
+    mse = np.mean((y_test - y_test_pred) ** 2)
+    print(f"best_tau = {best_tau}, mse under best_tau is {mse}")
+    plt.figure(f"best_tau = {best_tau}")
+    plt.plot(x_train[:, 1], y_train, 'bx', label='Training Data')
+    plt.plot(x_test[:, 1], y_test, 'go', label='Test Data')
+    plt.plot(x_test[:, 1], y_test_pred, 'ro', label='LWR Predictions on test data')
+    plt.legend()
+    plt.show()
+    # Save predictions to pred_path  def plot(x, y, theta, save_path, correction=1.0):
     # Plot data
     # *** END CODE HERE ***
 
