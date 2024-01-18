cc26e0b4afb9725f67cef27d777766238f8ff3be
ultraspicy <element.no.88@gmail.com>
2024-01-18 10:34:53 -0800

    Refine the plot for ps1::featuremaps::featuremap.py. Still need to make the curve cover the entire input span.

--- ps1/src/featuremaps/featuremap.py
+++ ps1/src/featuremaps/featuremap.py
@@ -71,7 +71,7 @@ class LinearModel(object):
             poly_features_with_sin[:, i] = X[:, 1] ** i
         poly_features_with_sin[:, k + 1] = np.sin(X[:, 1])
         
-        util.print_matrix(poly_features_with_sin)
+        #util.print_matrix(poly_features_with_sin)
         return poly_features_with_sin
 
         # *** END CODE HERE ***
@@ -97,65 +97,65 @@ class LinearModel(object):
         # *** END CODE HERE ***
 
 
-def run_exp(train_path, eval_path, sine=False, ks=[1, 2, 3, 5, 10, 20], filename='plot.png'):
+def run_exp(train_path, eval_path, sine=False, ks=[0, 1, 2, 3, 5, 10, 20], filename='plot.png'):
     train_x,train_y=util.load_dataset(train_path,add_intercept=True)
     plot_x = np.ones([1000, 2])
     plot_x[:, 1] = np.linspace(-factor*np.pi, factor*np.pi, 1000)
     plt.figure()
     plt.scatter(train_x[:, 1], train_y)
 
+    # *** START CODE HERE ***
+
+    # Load validation set and use validation poly features
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    print(f"x_eval.shape =  {x_eval.shape}")
+    x_eval_smooth = np.linspace(min(x_eval[:, 1]), max(x_eval[:, 1]), 1000).reshape(-1, 1)
+    x_eval_smooth = np.hstack([np.ones_like(x_eval_smooth), x_eval_smooth])
+    print(f"x_eval_smooth.shape =  {x_eval_smooth.shape}")
+
     for k in ks:
         '''
         Our objective is to train models and perform predictions on plot_x data
         '''
-        # *** START CODE HERE ***
-        
+        print(f"k = {k}")
         # Load training set
         x_train, y_train = util.load_dataset(train_path, add_intercept=True)
         print(f"shape of x_train {x_train.shape}")
         print(f"shape of y_train {y_train.shape}")
         # Fit a LWR model
         model = LinearModel()
-
         # convert the x_train into poly features
-        #x_train_poly
         if not sine:
-            x_train_poly =  model.create_poly(k, x_train)
+            x_train =  model.create_poly(k, x_train)
         else:
-            x_train_poly =  model.create_sin(k, x_train)
-        
-        model.fit(x_train_poly, y_train)
-        # Load validation set and use validation poly features
-        x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
-        print(f"x_eval.shape =  {x_eval.shape}")
-        print(f"y_eval.shape = {y_eval.shape}")
+            x_train =  model.create_sin(k, x_train)
+        model.fit(x_train, y_train)
         if not sine:
-            x_eval_poly =  model.create_poly(k, x_eval)
+            x_eval_smooth =  model.create_poly(k, x_eval_smooth)
         else:
-            x_eval_poly =  model.create_sin(k, x_eval)
-        y_pred = model.predict(x_eval_poly)
-        # Get MSE value on the validation set
-        mse = np.mean((y_eval - y_pred) ** 2)
-        print(f'MSE on validation set: {mse}')  
-        # *** END CODE HERE ***
+            x_eval_smooth =  model.create_sin(k, x_eval_smooth)
+
+        print(f"x_eval_smooth.shape =  {x_eval_smooth.shape}")
+        y_pred = model.predict(x_eval_smooth)
+    
         '''
         Here plot_y are the predictions of the linear model on the plot_x data
         '''
         plt.ylim(-2, 2)
-        # plt.plot(plot_x[:, 1], plot_y, label='k=%d' % k)
         if k == 1:
-            plt.plot(x_eval[:, 1], y_pred, 'cx', label='k=%d' % k)
+            plt.plot(x_eval_smooth[:, 1], y_pred, 'c-', label='k=%d' % k)
         elif k == 2:
-            plt.plot(x_eval[:, 1], y_pred, 'go', label='k=%d' % k)
+            plt.plot(x_eval_smooth[:, 1], y_pred, 'g-', label='k=%d' % k)
         elif k == 3:
-            plt.plot(x_eval[:, 1], y_pred, 'ro', label='k=%d' % k)
+            plt.plot(x_eval_smooth[:, 1], y_pred, 'r-', label='k=%d' % k)
         elif k == 5:
-            plt.plot(x_eval[:, 1], y_pred, '^k', label='k=%d' % k)
+            plt.plot(x_eval_smooth[:, 1], y_pred, 'k-', label='k=%d' % k)
         elif k == 10:
-            plt.plot(x_eval[:, 1], y_pred, 'b+', label='k=%d' % k)
+            plt.plot(x_eval_smooth[:, 1], y_pred, 'b-', label='k=%d' % k)
         elif k == 20:
-            plt.plot(x_eval[:, 1], y_pred, 'xm', label='k=%d' % k)
+            plt.plot(x_eval_smooth[:, 1], y_pred, 'm-', label='k=%d' % k)
 
+    # *** END CODE HERE ***
     plt.legend()
     plt.savefig(filename)
     plt.clf()
@@ -166,9 +166,10 @@ def main(train_path, small_path, eval_path):
     Run all expetriments
     '''
     # this is for (b)
-    # run_exp_k_equals_3(train_path, small_path, eval_path)
+    #run_exp_k_equals_3(train_path, small_path, eval_path)
     # this is for (c)
-    # run_exp(train_path, eval_path)
+    #run_exp(train_path, eval_path)
+    # this is for (d)
     run_exp(train_path, eval_path, True)
 
     
@@ -182,23 +183,23 @@ def run_exp_k_equals_3 (train_path, small_path, eval_path):
     # Fit a LWR model
     model = LinearModel()
 
-    # convert the x_train into poly features
+    # convert the x_train into poly features then train the model
     x_train_poly =  model.create_poly(3, x_train)
     model.fit(x_train_poly, y_train)
-    # Load validation set and use validation poly features
-    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
-    print(f"x_eval.shape =  {x_eval.shape}")
-    print(f"y_eval.shape = {y_eval.shape}")
-    x_eval_poly = model.create_poly(3, x_eval)
-    y_pred = model.predict(x_eval_poly)
-    # Get MSE value on the validation set
-    mse = np.mean((y_eval - y_pred) ** 2)
-    print(f'MSE on validation set: {mse}') 
-    # Plot validation predictions on top of training set  def plot(x, y, theta, save_path, correction=1.0):
+    # we need to plot the learnt hypothesis as a smooth curve over it
+    x_eval, _ = util.load_dataset(eval_path, add_intercept=True)
+    print(f"shape of x_eval {x_eval.shape}")
+    x_smooth = np.linspace(min(x_eval[:, 1]), max(x_eval[:, 1]), 1000).reshape(-1, 1)
+    x_smooth = np.hstack([np.ones_like(x_smooth), x_smooth])
+    print(f"shape of x_smooth {x_smooth.shape}")
+    x_poly_smooth = model.create_poly(3, x_smooth)
+    y_pred_smooth = model.predict(x_poly_smooth)
+    
+    # create a scatter plot of the training data
     plt.figure()
-    plt.plot(x_train[:, 1], y_train, 'bx', label='Training Data')
-    plt.plot(x_eval[:, 1], y_eval, 'go', label='Validation Data')
-    plt.plot(x_eval[:, 1], y_pred, 'ro', label='LWR Predictions')
+    plt.plot(x_train[:, 1], y_train, 'bx', label='training data')
+    # we need to plot the learnt hypothesis as a smooth curve over it
+    plt.plot(x_poly_smooth[:, 1], y_pred_smooth, 'r-', label='learnt hypothesis')
     plt.legend()
     plt.show()
     # *** END CODE HERE ***    
