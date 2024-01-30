0423687b6cb349f8ba1bce626d9b6c070167e8f0
ultraspicy <element.no.88@gmail.com>
2024-01-30 11:36:05 -0800

    Add solution code & tex for ps2::q1

--- ps2/src/poisson/poisson.py
+++ ps2/src/poisson/poisson.py
@@ -16,7 +16,24 @@ def main(lr, train_path, eval_path, save_path):
 
     # *** START CODE HERE ***
     # Fit a Poisson Regression model
-    # Run on the validation set, and use np.savetxt to save outputs to save_path
+    model = PoissonRegression()
+    model.fit(x_train, y_train)
+    
+    # Run on the validation set
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    y_pred = model.predict(x_eval)
+    # and use np.savetxt to save outputs to save_path
+    np.savetxt(save_path, y_pred)
+    # plot
+    plt.figure(figsize=(8, 6))
+    plt.scatter(y_eval, y_pred, alpha=0.5)
+    plt.xlim(0, 25)
+    plt.ylim(0, 25)
+    plt.xlabel('True Counts')
+    plt.ylabel('Predicted Counts')
+    plt.title('True Counts vs. Predicted Counts')
+    plt.savefig("ps2::q1::(d).png")
+    plt.show() 
     # *** END CODE HERE ***
 
 
@@ -45,6 +62,9 @@ class PoissonRegression:
         self.eps = eps
         self.verbose = verbose
 
+        ############################
+        self.learning_rate=0.01
+
     def fit(self, x, y):
         """Run gradient ascent to maximize likelihood for Poisson regression.
         Update the parameter by step_size * (sum of the gradient over examples)
@@ -54,6 +74,31 @@ class PoissonRegression:
             y: Training example labels. Shape (n_examples,).
         """
         # *** START CODE HERE ***
+        n = x.shape[0]
+        d = x.shape[1]
+        if self.theta is None:
+            self.theta = np.zeros(d)
+
+        print(f"self.theta.shape = {self.theta.shape}")
+        for i in range(self.max_iter): #range(100): #
+            # for jth sample
+            grad = np.zeros(d)
+            for j in range(n):
+                # x_jth is a vector, y is a scalar
+                # x_jth = x[j,:]
+                # print(f"x[]j,: = {x[j,:].shape}")
+                # print(f"{np.exp(self.theta.T @ x[j,:])}")
+                grad = grad + y[j] * (1 / np.exp(self.theta.T @ x[j,:])) * x[j,:] - x[j,:] 
+            grad = grad / n
+            self.theta = self.theta + self.learning_rate * grad
+            # print(f"grad = {grad}")
+            if np.linalg.norm(grad) < self.eps:
+                print(f"LR converge at i = {i}")
+                break
+            if self.verbose and i % 1000 == 0:
+                print(f"grad = {grad}")
+                print(f"self.theta = {self.theta}")
+                print(f"np.linalg.norm(grad) = {np.linalg.norm(grad)}")
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -66,6 +111,10 @@ class PoissonRegression:
             Floating-point prediction for each input, shape (n_examples,).
         """
         # *** START CODE HERE ***
+        # print(f"theta = {self.theta}")
+        # print(f"{self.theta.T.shape}")
+        # print(f"{x.shape}")
+        return np.exp(x @ self.theta.T)
         # *** END CODE HERE ***
 
 if __name__ == '__main__':
